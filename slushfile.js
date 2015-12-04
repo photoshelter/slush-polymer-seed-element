545e50ef08827931a81ce607733a74ca13838fad
Westbrook Johnson <westbrook.johnson@gmail.com>
2015-12-04 10:57:40 -0500

    target new folder

--- slushfile.js
+++ slushfile.js
@@ -100,7 +100,7 @@ gulp.task('default', function (done) {
                     }
                 }))
                 .pipe(conflict('./'))
-                .pipe(gulp.dest('./'))
+                .pipe(gulp.dest('./' + answers.elementName))
                 .pipe(install())
                 .on('end', function () {
                     done();
